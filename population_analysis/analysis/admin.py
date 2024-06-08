6de7f852c4e6b64b4f0f7d7199c031391ec255ca
Roman <romanfen1997@gmail.com>
2024-06-08 22:11:29 +0200

    1. Add population total data do admin panel 2. Add initial test template for the homepage

--- population_analysis/analysis/admin.py
+++ population_analysis/analysis/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from analysis.models import PopulationData
+
+
 # Register your models here.
+@admin.register(PopulationData)
+class PopulationDataAdmin(admin.ModelAdmin):
+    list_display = ('country_name', 'country_code', 'year_1960', 'year_2022')
+    search_fields = ('country_name', 'country_code', 'year_1960', 'year_2022')
+    ordering = ('country_name', 'year_1960', 'year_2022',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('country_name', 'country_code')
+        }),
+        ('Years', {
+            'classes': ('collapse',),
+            'fields': (
+                'year_1960', 'year_1961', 'year_1962', 'year_1963', 'year_1964',
+                'year_1965', 'year_1966', 'year_1967', 'year_1968', 'year_1969',
+                'year_1970', 'year_1971', 'year_1972', 'year_1973', 'year_1974',
+                'year_1975', 'year_1976', 'year_1977', 'year_1978', 'year_1979',
+                'year_1980', 'year_1981', 'year_1982', 'year_1983', 'year_1984',
+                'year_1985', 'year_1986', 'year_1987', 'year_1988', 'year_1989',
+                'year_1990', 'year_1991', 'year_1992', 'year_1993', 'year_1994',
+                'year_1995', 'year_1996', 'year_1997', 'year_1998', 'year_1999',
+                'year_2000', 'year_2001', 'year_2002', 'year_2003', 'year_2004',
+                'year_2005', 'year_2006', 'year_2007', 'year_2008', 'year_2009',
+                'year_2010', 'year_2011', 'year_2012', 'year_2013', 'year_2014',
+                'year_2015', 'year_2016', 'year_2017', 'year_2018', 'year_2019',
+                'year_2020', 'year_2021', 'year_2022'
+            ),
+        }),
+    )
